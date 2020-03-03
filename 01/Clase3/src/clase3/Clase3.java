a049926407653874921764ad8f8e66aa45713ef7
SebastianAlexanderNarvaezUsama <narvaezusamasebastianalexander@gmail.com>
2020-03-03 14:55:05 -0500

    Calculadora basica, error en ejercio 3, operaciones y ciclos.

--- 01/Clase3/src/clase3/Clase3.java
+++ 01/Clase3/src/clase3/Clase3.java
@@ -6,7 +6,9 @@
 package clase3;
 
 import java.util.Scanner;
-import java.text.DecimalFormat;
+/**/
+
+
 
 /**
  *
@@ -18,24 +20,44 @@ public class Clase3 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        
-        
-        
         Scanner reader = new Scanner(System.in);
+      /*  
+        int i=0, p=0;
+        for (i=1 ; i<=30 ; i++){
+           System.out.println(i);
+
+           if(i%2 == 0)
+                   p++;
+         System.out.println(p);  
+
+        }
+    */
+    
+        
+        
+        
         
         int N1, N2, op;
         
+
+     
         System.out.println("Escriba el primer numero: ");
         N1 = reader.nextInt();
         System.out.println("Escriba el segundo numero: ");
         N2 = reader.nextInt();        
         System.out.flush();
+        
+        
+        
+        
         System.out.println("******************MENU******************");
         System.out.println("Suma de los numeros digite:             1");
         System.out.println("Resta de los numeros digite:            2");
         System.out.println("Multiplicacion de los numeros digite:   3");
         System.out.println("Divicion de los numeros digite:         4");
-        op = reader.nextInt();  
+        System.out.println("Salir:                                  5");
+        op = reader.nextInt(); 
+        
         System.out.flush();
         switch (op) {
             case 1:
@@ -50,10 +72,17 @@ public class Clase3 {
             case 4:
                 System.out.println("La divisiòn de los numeros es: " + (N1/N2)); 
                 break;
+            case 5:
+                System.out.println("VUELVA PRONTO");
+                System.out.close();
+               break;
+            default:
+                System.out.println("*************ERROR************");
+                System.out.println("*******DATOS NO VALIDOS*******");
+                break;  
                 
-               
-
-        /* Ejercicio 1
+                
+        /* ---------------------------------------------------------------------Ejercicio 1
         if (N1>N2){
         System.out.print("El mayor es: "+ N1);
         }else
@@ -64,13 +93,38 @@ public class Clase3 {
         System.out.print("Los numeros " + N1 + " y " + N2 + " son iguales " );
         }
          */
-            default:      
-                break;
-        }
-           } 
+                 
+        /*-----------------------------------------------------------------------Ejercicio 2
+          int N1=5, N2=6, OP=6, Res=0;
+          //Generacion aritmetica
+          if (OP==1)
+              Res = N1+N2;
+          else  
+               if (OP==2)
+                   Res = N1-N2;
+               else 
+                   if (OP==3)
+                       Res = N1*N2;
+                   else 
+                       if (OP==4)
+                          Res = N1/N2;
+                          else
+                             System.out.println("Incorrecto");
+                             System.out.close(); 
+
+          System.out.println("Resultado: "+Res );                                  
+          */    
           
-        }
-     
-        // TODO code application logic here
+            
+    
+           
+        
+    }
+}
+}        // TODO code application logic here
+    
+    
+
+// TODO code application logic here
     
     
