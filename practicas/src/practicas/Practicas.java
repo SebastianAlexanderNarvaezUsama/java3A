16a8434b843bec53bef98e4393729b762c3e6001
SebastianAlexanderNarvaezUsama <narvaezusamasebastianalexander@gmail.com>
2020-03-24 14:40:51 -0500

    practica, mostrar conteo del 1 al 10 con inicio de variable en 1 y 0

--- practicas/src/practicas/Practicas.java
+++ practicas/src/practicas/Practicas.java
@@ -15,7 +15,10 @@ public class Practicas {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+
+        for (int i=0;i<10;i++){
+            System.out.println("Numero: "+(i+1));
+        }
     }
     
 }
