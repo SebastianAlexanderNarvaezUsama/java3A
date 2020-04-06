9ccc0bebdd54378769a81c50a73939aace6413d0
SebastianAlexanderNarvaezUsama <narvaezusamasebastianalexander@gmail.com>
2020-04-05 22:48:56 -0500

    MostrarNombre, Reto Completo, mostar nombre en mayus, minus, primeras mayus y total de caracteres, buscador completo, botones mayus y minus completos.

--- MostrarNombre/src/mostrarnombre/MostrarNombre.java
+++ MostrarNombre/src/mostrarnombre/MostrarNombre.java
@@ -15,7 +15,16 @@ public class MostrarNombre {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        String Nombre="sebastian alexander Narvaez Usama";
+          String Palabra=Nombre;
+          int numero=32;
+    char Buscarletra [];
+    Buscarletra = Palabra.toCharArray();
+    for (int x=0;x<Palabra.length();x++){
+        if(numero==x){
+        System.out.println(Buscarletra [x]);    
+        }
+        }  // TODO code application logic here
     }
     
 }
