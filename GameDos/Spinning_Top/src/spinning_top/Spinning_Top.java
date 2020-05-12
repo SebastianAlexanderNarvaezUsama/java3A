45f2935103257957b6c92c53a1050adbf96ea8f1
carolina <caritobotina1@gmail.com>
2020-05-12 13:59:43 -0500

    Turns in console

--- GameDos/Spinning_Top/src/spinning_top/Spinning_Top.java
+++ GameDos/Spinning_Top/src/spinning_top/Spinning_Top.java
@@ -10,12 +10,20 @@ package spinning_top;
  * @author Asus
  */
 public class Spinning_Top {
-
+    
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        int turn=1;//turno
+        while(turn<=4){//el ciclo solo es para ver la efectividad de los turnos.
+        System.out.println("turno "+turn);
+        if(turn==4){
+            turn-=4;
+        }
+        turn++;
+        }
     }
     
 }
