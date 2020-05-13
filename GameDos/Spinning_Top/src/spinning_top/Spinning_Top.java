e48cd751d5a31f291235c7c8f680406d4d63506e
SebastianAlexanderNarvaezUsama <narvaezusamasebastianalexander@gmail.com>
2020-05-13 01:23:31 -0500

    Diseño de pantalla ConficZone completa con validaciones, aun falta validar puntage con variable interna.

--- GameDos/Spinning_Top/src/spinning_top/Spinning_Top.java
+++ GameDos/Spinning_Top/src/spinning_top/Spinning_Top.java
@@ -5,17 +5,97 @@
  */
 package spinning_top;
 
+import java.util.Random;
+import java.util.Scanner;
+
 /**
  *
  * @author Asus
  */
 public class Spinning_Top {
 
+    static void CarasPirinola(){
+        Random ladosp=new Random();
+        int caras=(int)(ladosp.nextDouble()*6+1);
+        switch(caras){
+            case 1:
+                System.out.println("Pon 1");
+                break;
+            case 2:
+                System.out.println("Pon 2");
+                break;
+            case 3:
+                System.out.println("Toma 1");
+                break;
+            case 4:
+                System.out.println("Toma 2");
+                break;
+            case 5:
+                System.out.println("Toma todo");
+                break;
+            case 6:
+                System.out.println("Todos ponen");
+        }    
+    }
+    
+    static void Menu(){
+        Scanner reader=new Scanner(System.in);
+        int coins1=5,coins2=10,coins3=20,coins4=30;
+        System.out.println("Menú");
+        System.out.println("1. 5 coins");
+        System.out.println("2. 10 coins");
+        System.out.println("3. 20 coins");
+        System.out.println("4. 30 coins");
+        System.out.println("Elige una opción: ");
+        int opcion=reader.nextInt();
+        switch(opcion){
+            case 1:
+                System.out.println("Bienvenido al nivel 1");
+                for(int turn=1;turn<5;turn++){
+                    System.out.println("Player "+turn);
+                    CarasPirinola();
+                }
+                break;
+            case 2:
+                System.out.println("Bienvenido al nivel 2");
+                for(int turn=1;turn<5;turn++){
+                    System.out.println("Player "+turn);
+                    CarasPirinola();
+                }
+                break;
+            case 3:
+                System.out.println("Bienvenido al nivel 3");
+                for(int turn=1;turn<5;turn++){
+                    System.out.println("Player "+turn);
+                    CarasPirinola();
+                }
+                break;
+            case 4:
+                System.out.println("Bienvenido al nivel 4");
+                for(int turn=1;turn<5;turn++){
+                    System.out.println("Player "+turn);
+                    CarasPirinola();
+                }
+                break;
+            default:
+                break;
+        }
+
+    }
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        // Sistema de turnos en consola
+        Menu();
+        /*for(int turn=0;turn<4;turn++){
+            if(turn==4){
+                turn-=3;
+            }
+            System.out.println("Turno: "+(turn+1));*/
+        
+        
+        }
     }
     
-}
+
