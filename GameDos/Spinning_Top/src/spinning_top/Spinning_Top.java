9c8fec48ef621a0d4c4326366535a38e5bd48cba
carolina <caritobotina1@gmail.com>
2020-05-13 23:45:10 -0500

    Game logic in process

--- GameDos/Spinning_Top/src/spinning_top/Spinning_Top.java
+++ GameDos/Spinning_Top/src/spinning_top/Spinning_Top.java
@@ -13,16 +13,18 @@ import java.util.Scanner;
  * @author Asus
  */
 public class Spinning_Top {
-
     static void CarasPirinola(){
+        int coins1=5,coins=0;
         Random ladosp=new Random();
         int caras=(int)(ladosp.nextDouble()*6+1);
         switch(caras){
             case 1:
                 System.out.println("Pon 1");
+                coins1-=1;
                 break;
             case 2:
                 System.out.println("Pon 2");
+                coins1-=2;
                 break;
             case 3:
                 System.out.println("Toma 1");
@@ -35,12 +37,14 @@ public class Spinning_Top {
                 break;
             case 6:
                 System.out.println("Todos ponen");
+                coins1-=1;
+                break;
         }    
     }
     
     static void Menu(){
         Scanner reader=new Scanner(System.in);
-        int coins1=5,coins2=10,coins3=20,coins4=30;
+        int coins1=5,coins2=10,coins3=20,coins4=30,coins=0;
         System.out.println("Menú");
         System.out.println("1. 5 coins");
         System.out.println("2. 10 coins");
@@ -48,12 +52,20 @@ public class Spinning_Top {
         System.out.println("4. 30 coins");
         System.out.println("Elige una opción: ");
         int opcion=reader.nextInt();
+        //do{
         switch(opcion){
             case 1:
                 System.out.println("Bienvenido al nivel 1");
+                CarasPirinola();
+                while(coins==0){
                 for(int turn=1;turn<5;turn++){
                     System.out.println("Player "+turn);
                     CarasPirinola();
+                    System.out.println("Coins Player "+turn+": "+Monedas(coins1));
+                    System.out.println("---------------------");
+                    System.out.println("/////////////////////");
+                    Esperar();
+                }
                 }
                 break;
             case 2:
@@ -61,6 +73,9 @@ public class Spinning_Top {
                 for(int turn=1;turn<5;turn++){
                     System.out.println("Player "+turn);
                     CarasPirinola();
+                    System.out.println("---------------------");
+                    System.out.println("/////////////////////");
+                    Esperar();
                 }
                 break;
             case 3:
@@ -68,6 +83,9 @@ public class Spinning_Top {
                 for(int turn=1;turn<5;turn++){
                     System.out.println("Player "+turn);
                     CarasPirinola();
+                    System.out.println("---------------------");
+                    System.out.println("/////////////////////");
+                    Esperar();
                 }
                 break;
             case 4:
@@ -75,26 +93,60 @@ public class Spinning_Top {
                 for(int turn=1;turn<5;turn++){
                     System.out.println("Player "+turn);
                     CarasPirinola();
+                    System.out.println("---------------------");
+                    System.out.println("/////////////////////");
+                    Esperar();
                 }
                 break;
             default:
                 break;
         }
+        //}while(coins1==0);
 
     }
+    
+    static int Monedas(int coins1){
+        int coins = 0;
+        Random ladosp=new Random();
+        int caras=(int)(ladosp.nextDouble()*6+1);
+        switch (caras) {
+            case 1:
+                coins=coins1-1;
+                break;
+            case 2:
+                coins=coins1-2;
+                break;
+            case 3:
+                break;
+            case 4:
+                break;
+            case 5:
+                break;
+            case 6:
+                coins1-=1;
+                break;
+        }
+        return coins;
+    }
+    
+    static void Esperar(){
+        try{
+            Thread.sleep(5*1000);
+        }catch(Exception e){
+            System.out.println("ERROR");
+        }
+    }
+    
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // Sistema de turnos en consola
         Menu();
         /*for(int turn=0;turn<4;turn++){
             if(turn==4){
                 turn-=3;
             }
             System.out.println("Turno: "+(turn+1));*/
-        
-        
         }
     }
     
